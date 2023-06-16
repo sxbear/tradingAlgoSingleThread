33b178694453df0dfb38afaeaba8a1fe97fe73e3
sxbear <solomon.bear@proton.me>
2023-06-15 13:14:45 -1200

    Split and Improve
    Functions were split but also improvements were made and menus are put in place.

---
+++ out_cygwin/CMakeFiles/TASTSXB.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/cygdrive/C/Users/solom/Documents/Github/TradingAlgoSingleThread/src/config.cpp" "CMakeFiles/TASTSXB.dir/src/config.cpp.o" "gcc" "CMakeFiles/TASTSXB.dir/src/config.cpp.o.d"
+  "/cygdrive/C/Users/solom/Documents/Github/TradingAlgoSingleThread/src/dataParser.cpp" "CMakeFiles/TASTSXB.dir/src/dataParser.cpp.o" "gcc" "CMakeFiles/TASTSXB.dir/src/dataParser.cpp.o.d"
+  "/cygdrive/C/Users/solom/Documents/Github/TradingAlgoSingleThread/src/databaseManager.cpp" "CMakeFiles/TASTSXB.dir/src/databaseManager.cpp.o" "gcc" "CMakeFiles/TASTSXB.dir/src/databaseManager.cpp.o.d"
+  "/cygdrive/C/Users/solom/Documents/Github/TradingAlgoSingleThread/src/globalQuote.cpp" "CMakeFiles/TASTSXB.dir/src/globalQuote.cpp.o" "gcc" "CMakeFiles/TASTSXB.dir/src/globalQuote.cpp.o.d"
+  "/cygdrive/C/Users/solom/Documents/Github/TradingAlgoSingleThread/src/indicators.cpp" "CMakeFiles/TASTSXB.dir/src/indicators.cpp.o" "gcc" "CMakeFiles/TASTSXB.dir/src/indicators.cpp.o.d"
+  "/cygdrive/C/Users/solom/Documents/Github/TradingAlgoSingleThread/src/macd.cpp" "CMakeFiles/TASTSXB.dir/src/macd.cpp.o" "gcc" "CMakeFiles/TASTSXB.dir/src/macd.cpp.o.d"
+  "/cygdrive/C/Users/solom/Documents/Github/TradingAlgoSingleThread/src/main.cpp" "CMakeFiles/TASTSXB.dir/src/main.cpp.o" "gcc" "CMakeFiles/TASTSXB.dir/src/main.cpp.o.d"
+  "/cygdrive/C/Users/solom/Documents/Github/TradingAlgoSingleThread/src/marketDataAPI.cpp" "CMakeFiles/TASTSXB.dir/src/marketDataAPI.cpp.o" "gcc" "CMakeFiles/TASTSXB.dir/src/marketDataAPI.cpp.o.d"
+  "/cygdrive/C/Users/solom/Documents/Github/TradingAlgoSingleThread/src/strategy.cpp" "CMakeFiles/TASTSXB.dir/src/strategy.cpp.o" "gcc" "CMakeFiles/TASTSXB.dir/src/strategy.cpp.o.d"
+  "/cygdrive/C/Users/solom/Documents/Github/TradingAlgoSingleThread/src/tradingAPI.cpp" "CMakeFiles/TASTSXB.dir/src/tradingAPI.cpp.o" "gcc" "CMakeFiles/TASTSXB.dir/src/tradingAPI.cpp.o.d"
+  "/cygdrive/C/Users/solom/Documents/Github/TradingAlgoSingleThread/src/utility.cpp" "CMakeFiles/TASTSXB.dir/src/utility.cpp.o" "gcc" "CMakeFiles/TASTSXB.dir/src/utility.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ out_cygwin/CMakeFiles/TASTSXB.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TASTSXB.dir/src/config.cpp.o"
+  "CMakeFiles/TASTSXB.dir/src/config.cpp.o.d"
+  "CMakeFiles/TASTSXB.dir/src/dataParser.cpp.o"
+  "CMakeFiles/TASTSXB.dir/src/dataParser.cpp.o.d"
+  "CMakeFiles/TASTSXB.dir/src/databaseManager.cpp.o"
+  "CMakeFiles/TASTSXB.dir/src/databaseManager.cpp.o.d"
+  "CMakeFiles/TASTSXB.dir/src/globalQuote.cpp.o"
+  "CMakeFiles/TASTSXB.dir/src/globalQuote.cpp.o.d"
+  "CMakeFiles/TASTSXB.dir/src/indicators.cpp.o"
+  "CMakeFiles/TASTSXB.dir/src/indicators.cpp.o.d"
+  "CMakeFiles/TASTSXB.dir/src/macd.cpp.o"
+  "CMakeFiles/TASTSXB.dir/src/macd.cpp.o.d"
+  "CMakeFiles/TASTSXB.dir/src/main.cpp.o"
+  "CMakeFiles/TASTSXB.dir/src/main.cpp.o.d"
+  "CMakeFiles/TASTSXB.dir/src/marketDataAPI.cpp.o"
+  "CMakeFiles/TASTSXB.dir/src/marketDataAPI.cpp.o.d"
+  "CMakeFiles/TASTSXB.dir/src/strategy.cpp.o"
+  "CMakeFiles/TASTSXB.dir/src/strategy.cpp.o.d"
+  "CMakeFiles/TASTSXB.dir/src/tradingAPI.cpp.o"
+  "CMakeFiles/TASTSXB.dir/src/tradingAPI.cpp.o.d"
+  "CMakeFiles/TASTSXB.dir/src/utility.cpp.o"
+  "CMakeFiles/TASTSXB.dir/src/utility.cpp.o.d"
+  "TASTSXB.exe"
+  "TASTSXB.pdb"
+  "libTASTSXB.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TASTSXB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
